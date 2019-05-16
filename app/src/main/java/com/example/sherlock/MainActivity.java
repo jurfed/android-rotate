fc5a3d9d200443f97a28abcafae9961ec615d535
jur <jurfed@inbox.ru>
2019-05-16 12:01:38 +0300

    fitst commit

--- app/src/main/java/com/example/sherlock/MainActivity.java
+++ app/src/main/java/com/example/sherlock/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Button rotate;
     TextView textOrientation;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
